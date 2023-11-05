f411a186bb485d458398fc7c6aa373681476721f
seono9y <144989211+seono9y@users.noreply.github.com>
2023-11-05 20:44:17 +0900

    02-2

--- exercise_09/exercise_09/main.c
+++ exercise_09/exercise_09/main.c
@@ -10,13 +10,20 @@
 #define N_STUDENT 5
 int main(int argc, char *argv[1])
 {
-    int i;
+    int i, average;
+    int sum;
     int grade [N_STUDENT];
     printf ("input 5 scores:");
-    for (i=0;i<N_STUDENT; i++)
-        scanf ("%d", &grade[i]);
     
-    for (i=0;i<N_STUDENT;i++)
-        printf ("grade[%i] = %d\n", i, grade[i]);
+    sum=0;
+    for (i=0;i<N_STUDENT; i++)
+    {
+        scanf ("%d", &grade[i]);
+        sum += grade[i];
+    }
+    
+    average = sum / N_STUDENT;
+    
+        printf ("score average : %i\n", average);
     return 0;
  }
